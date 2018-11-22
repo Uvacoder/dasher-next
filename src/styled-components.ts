ac4b06d699fd2fd611f809e70540dbf86abe8ea2
Cole Bemis <colebemis@gmail.com>
2018-11-21 22:57:46 -0800

    Add newline

--- src/styled-components.ts
+++ src/styled-components.ts
@@ -1,4 +1,5 @@
 // Source: https://www.styled-components.com/docs/api#define-a-theme-interface
+
 import * as styledComponents from 'styled-components'
 import { ThemedStyledComponentsModule } from 'styled-components'
 import { Theme } from './theme'
