3f4a73114b8fb6adbec63bc6e8150561ee94557e
Cole Bemis <colebemis@gmail.com>
2018-11-23 00:36:08 -0800

    Add octicons

--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,5 +1,7 @@
+import Octicon, { GitPullRequest } from '@githubprimer/octicons-react'
 import React from 'react'
 import useTheme from '../utils/useTheme'
+import Base from './Base'
 import Flex from './Flex'
 import Link from './Link'
 
@@ -14,7 +16,16 @@ interface Issue {
 const Issue = ({ issue }: IssueProps) => {
   const theme = useTheme()
   return (
-    <Flex css={{ flexDirection: 'column', padding: theme.spacing[4] }}>
+    <Flex css={{ padding: theme.spacing[4] }}>
+      <Base
+        css={{
+          marginRight: theme.spacing[2],
+          lineHeight: theme.lineHeights.tight,
+          color: theme.colors.green[8],
+        }}
+      >
+        <Octicon icon={GitPullRequest} />
+      </Base>
       <Link
         css={{
           fontWeight: theme.fontWeights.medium,
