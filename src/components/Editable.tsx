03fc6bf69fe9cdd9c328636951257501c546289b
Cole Bemis <colebemis@gmail.com>
2018-11-22 22:48:51 -0800

    Remove react-contenteditable dependency

--- src/components/Editable.tsx
+++ src/components/Editable.tsx
@@ -1,33 +1,36 @@
 import React from 'react'
-import ContentEditable from 'react-contenteditable'
 import { CSSObject } from 'styled-components'
 import useTheme from '../utils/useTheme'
 import Base from './Base'
 
-// TODO: Expose `as` prop that maps to ContentEditable's `tagName` prop
-// TODO: Open PR to update `tagName` type to support `React.ComponentType<any>`
-// TODO: Open PR to update `onChange` and `onBlur` function signatures
-
 interface EditableProps {
   html: string
-  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>
+  as?: keyof JSX.IntrinsicElements
   css?: CSSObject
   disabled?: boolean
-  onChange?: (event: React.SyntheticEvent<any>) => void
-  onBlur?: (event: React.SyntheticEvent<any>) => void
+  spellCheck?: boolean
 }
 
-const Editable = ({ as = 'div', css, ...props }: EditableProps) => {
+const Editable = ({
+  as = 'div',
+  html,
+  css,
+  disabled = false,
+  spellCheck = true,
+}: EditableProps) => {
   const theme = useTheme()
 
   return (
     <Base
-      as={ContentEditable}
+      as={as}
+      contentEditable={!disabled}
+      dangerouslySetInnerHTML={{ __html: html }}
+      spellCheck={spellCheck}
       css={{
         borderRadius: theme.radii[1],
         outline: 0,
         '&:hover': {
-          backgroundColor: theme.colors.grayAlpha[2],
+          backgroundColor: theme.colors.grayAlpha[1],
         },
         '&:focus': {
           backgroundColor: theme.colors.white,
@@ -35,7 +38,6 @@ const Editable = ({ as = 'div', css, ...props }: EditableProps) => {
         },
         ...css,
       }}
-      {...props}
     />
   )
 }
