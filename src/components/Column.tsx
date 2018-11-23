8f657a26a5075955bf866cdd49050d7972568efc
Cole Bemis <colebemis@gmail.com>
2018-11-22 21:42:32 -0800

    Update Column header

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { CSSObject } from 'styled-components'
 import useTheme from '../utils/useTheme'
+import Badge from './Badge'
+import Base from './Base'
+import Code from './Code'
 import Flex from './Flex'
 
 interface ColumnProps {
@@ -26,15 +29,36 @@ const Column = ({ column, css }: ColumnProps) => {
         flexDirection: 'column',
         flexShrink: 0,
         width: '400px',
-        padding: theme.spacing[4],
         backgroundColor: theme.colors.white,
         borderRadius: theme.radii[3],
         boxShadow: '0 1px 1px 1px rgba(0, 35, 70, 0.09)',
         ...css,
       }}
     >
-      <span>{name}</span>
-      <span>{query}</span>
+      <Flex
+        css={{
+          padding: theme.spacing[4],
+          flexDirection: 'column',
+          alignItems: 'start',
+          borderBottom: `1px solid ${theme.colors.grayAlpha[2]}`,
+        }}
+      >
+        <Flex css={{ alignItems: 'center', margin: `0 0 ${theme.spacing[2]}` }}>
+          <Base
+            as="h2"
+            css={{
+              margin: 0,
+              fontSize: theme.fontSizes[4],
+              fontWeight: theme.fontWeights.semibold,
+              lineHeight: theme.lineHeights.tight,
+            }}
+          >
+            {name}
+          </Base>
+          <Badge>8</Badge>
+        </Flex>
+        <Code css={{ color: theme.colors.grayAlpha[7] }}>{query}</Code>
+      </Flex>
     </Flex>
   )
 }
